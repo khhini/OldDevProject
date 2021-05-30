9337812da77f8faa10b7366c4aee9efefeea6813
khh-ini <kiki18ti@mahasiswa.pcr.ac.id>
2021-05-31 01:06:38 +0700

    add prediction to Image and add error hendling on all endpoint

--- functions/laporans/laporan.js
+++ functions/laporans/laporan.js
@@ -1,6 +1,7 @@
 const admin = require("firebase-admin");
 const { body, validationResult } = require("express-validator");
 const mime = require("mime-types");
+const http = require("http");
 
 exports.validators = [
   body("alamat").notEmpty().withMessage("alamat harus diisi"),
@@ -15,77 +16,103 @@ exports.validators = [
 ];
 
 exports.create = async (req, res) =>{
-  const { authorization } = req.headers;
-  const split = authorization.split(" ");
-  const token = split[1];
-  const decodeToken = await admin.auth().verifyIdToken(token);
+  try {
+    const { authorization } = req.headers;
+    const split = authorization.split(" ");
+    const token = split[1];
+    const decodeToken = await admin.auth().verifyIdToken(token);
 
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() });
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    // eslint-disable-next-line prefer-const
+    let { foto, ...data } = req.body;
+    const mimeType = base64MimeType(req.body.foto);
+    data.id_pelapor = decodeToken.uid;
+
+
+    const { id } = await admin.firestore().collection("laporans").add(data);
+
+    const publicUrl = await uploadAndGetPublicFile(
+        id +"." + mime.extension(mimeType),
+        foto
+    );
+
+    data.foto = publicUrl;
+
+    await admin.firestore().collection("laporans").doc(id).update(data);
+
+    requestPrediction(id, foto, data);
+
+    return res.status(201).send(JSON.stringify({ id }));
+  } catch (err) {
+    return handleError(res, err);
   }
-  // eslint-disable-next-line prefer-const
-  let data = req.body;
-  const mimeType = base64MimeType(req.body.foto);
-  data.id_pelapor = decodeToken.uid;
-
-
-  const { id } = await admin.firestore().collection("laporans").add(data);
-
-  const publicUrl = await uploadAndGetPublicFile(
-      id +"." + mime.extension(mimeType),
-      req.body.foto
-  );
-
-  data.foto = publicUrl;
-
-  await admin.firestore().collection("laporans").doc(id).update(data);
-
-  return res.status(201).send(JSON.stringify({ id }));
 };
 
 
 exports.all = async (req, res) => {
-  const snapshot = await admin.firestore().collection("laporans").get();
+  try {
+    const snapshot = await admin.firestore().collection("laporans").get();
 
-  // eslint-disable-next-line prefer-const
-  let data = [];
-  snapshot.forEach((doc) => {
-    const id = doc.id;
-    const d = doc.data();
+    // eslint-disable-next-line prefer-const
+    let data = [];
+    snapshot.forEach((doc) => {
+      const id = doc.id;
+      const d = doc.data();
 
-    data.push({ id, ...d });
-  });
+      data.push({ id, ...d });
+    });
 
-  return res.status(200).send(JSON.stringify(data));
+    return res.status(200).send(JSON.stringify(data));
+  } catch (err) {
+    return handleError(res, err);
+  }
 };
 
 exports.get = async (req, res) => {
-  const snapshot = await admin.firestore()
-      .collection("laporans").doc(req.params.id).get();
+  try {
+    const snapshot = await admin.firestore()
+        .collection("laporans").doc(req.params.id).get();
 
-  const id = snapshot.id;
-  const data = snapshot.data();
+    const id = snapshot.id;
+    const data = snapshot.data();
 
-  if (!data) {
-    return res.status(404).send("data not found");
+    if (!data) {
+      return res.status(404).send("data not found");
+    }
+
+    return res.status(200).send(JSON.stringify({ id: id, ...data }));
+  } catch (err) {
+    return handleError(res, err);
   }
-
-  return res.status(200).send(JSON.stringify({ id: id, ...data }));
 };
 
 
 exports.remove = async (req, res) => {
-  // delete Image from Cloud Storage
-  const snapshot = await admin.firestore()
-      .collection("laporans").doc(req.params.id).get();
-  let { foto } = snapshot.data();
-  foto = foto.replace(/.+public\//, "");
-  deleteImageFromStorage(foto).catch(console.error);
-  // delete data from Firestore
-  await admin.firestore()
-      .collection("laporans").doc(req.params.id).delete();
-  return res.status(204).send();
+  try {
+    // delete Image from Cloud Storage
+    const snapshot = await admin.firestore()
+        .collection("laporans").doc(req.params.id).get();
+
+    // eslint-disable-next-line camelcase
+    let { foto, predicted_foto } = snapshot.data();
+    foto = foto.replace(/.+public\//, "");
+
+    // eslint-disable-next-line camelcase
+    predicted_foto = predicted_foto.replace(/.+public\//, "");
+    deleteImageFromStorage(foto);
+    deleteImageFromStorage(predicted_foto);
+
+    // delete data from Firestore
+    await admin.firestore()
+        .collection("laporans").doc(req.params.id).delete();
+
+    return res.status(204).send();
+  } catch (err) {
+    return handleError(res, err);
+  }
 };
 
 const deleteImageFromStorage = async (fileName) => {
@@ -123,3 +150,54 @@ function base64MimeType(encoded) {
 
   return result;
 }
+
+function handleError(res, err) {
+  return res.status(500).send({ message: `${err.code} - ${err.message}` });
+}
+
+const requestPrediction = (id, foto, data) =>{
+  foto = JSON.stringify({ foto });
+
+  const options = {
+    hostname: "localhost",
+    port: 5000,
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Content-Length": foto.length,
+    },
+  };
+
+  const predictReq = http.request(options, (response) => {
+    console.log(`statusCode: ${response.statusCode}`);
+    let responseData = "";
+    response.on("data", (d) => {
+      responseData += d;
+    });
+
+    response.on("end", async () =>{
+      try {
+        responseData = JSON.parse(responseData);
+        const fileFoto = responseData.predicted_image;
+        const fileMimeType = base64MimeType(fileFoto);
+        const fileExtention = mime.extension(fileMimeType);
+        const predictedUrl = await uploadAndGetPublicFile(
+            id + "-predicted." + fileExtention,
+            fileFoto
+        );
+        data.predicted_foto = predictedUrl;
+        await admin.firestore().collection("laporans").doc(id).update(data);
+        console.log("Image Prediction Complete: "+ predictedUrl);
+      } catch ( err ) {
+        console.error( err );
+      }
+    });
+  });
+
+  predictReq.on("error", (error) => {
+    console.error(error);
+  });
+
+  predictReq.write(foto);
+  predictReq.end();
+};
